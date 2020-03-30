762c70a38f51802c2228830a9f9f97d0a56a8f8a
theuitown <nitingoswami473@gmail.com>
2020-03-30 20:21:10 +0530

    animations added

--- saxxtext/lib/saxxtext.js
+++ saxxtext/lib/saxxtext.js
@@ -1,1 +1,44 @@
-function setAnimationSaxx(t,e){var s,o;s=t,o=e,setTimeout(function(){var t,e=document.querySelectorAll(s);for(t=0;t<e.length;++t)e[t].removeAttribute("class",o)},900),function(t,e){var s,o=document.querySelectorAll(t);for(s=0;s<o.length;++s)o[s].setAttribute("class",e)}(t,e)}function setSaxxMouseEffect(t,e,s,o){var r=document.querySelectorAll(t);for(i=0;i<r.length;++i)r[i].addEventListener("mouseover",function(){this.style.color=o,this.style.cursor="default",this.setAttribute("class",e)}),r[i].addEventListener("hover",function(){this.style.color=o,this.style.cursor="default",this.setAttribute("class",e)}),r[i].addEventListener("mousemove",function(){this.style.color=o,this.style.cursor="default",this.setAttribute("class",e)}),r[i].addEventListener("mouseout",function(){this.style.color=s,this.style.cursor="default",this.removeAttribute("class",e)})}
+function setAnimationSaxx(elem,saxxAnimationName){
+  function addAnimationSaxx(elem,saxxAnimationName) {
+        var element=document.querySelectorAll(elem),i;
+            for (i = 0; i < element.length; ++i) {
+              element[i].setAttribute('class',saxxAnimationName);
+            }
+            }
+
+function rmAnimationSaxx(elem,saxxAnimationName) {
+     setTimeout(function(){
+        var element=document.querySelectorAll(elem),i;
+            for (i = 0; i < element.length; ++i) {
+          element[i].removeAttribute('class',saxxAnimationName);
+            }
+            },900);
+        }
+
+  rmAnimationSaxx(elem,saxxAnimationName);
+  addAnimationSaxx(elem,saxxAnimationName);
+}
+
+function setSaxxMouseEffect(elem,saxxAnimationName,textColorbefore,textColorAfter){
+
+  var element=document.querySelectorAll(elem);
+  for (i = 0; i < element.length; ++i) {
+      element[i].addEventListener('mouseover',function(){
+    this.style.color=textColorAfter;
+    this.style.cursor='default';
+    this.setAttribute('class',saxxAnimationName);
+  });
+
+  element[i].addEventListener('hover',function(){a
+this.style.color=textColorAfter;
+this.style.cursor='default';
+this.setAttribute('class',saxxAnimationName);
+});
+    
+         element[i].addEventListener('mouseout',function(){
+    this.style.color=textColorbefore;
+    this.style.cursor='default';
+    this.removeAttribute('class',saxxAnimationName);
+  });
+  }
+}
